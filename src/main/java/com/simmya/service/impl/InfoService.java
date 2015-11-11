af452e3888835849007e23150328de6cd78b6464
ginger-k <ywkxwx@163.com>
2015-11-11 23:30:31 +0800

    天剑的点点滴滴

---
+++ src/main/java/com/simmya/service/impl/InfoService.java
@@ -0,0 +1,11 @@
+package com.simmya.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.simmya.pojo.Info;
+import com.simmya.service.BaseService;
+
+@Service
+public class InfoService extends BaseService<Info>{
+
+}
