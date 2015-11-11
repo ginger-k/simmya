fcc62ed503571a24aa24b4539b2c7e36d721f0ad
ginger-k <ywkxwx@163.com>
2015-11-11 22:26:43 +0800

    继续完善通用mapper

--- src/main/java/com/simmya/mapper/DiscussMapper.java
+++ src/main/java/com/simmya/mapper/DiscussMapper.java
@@ -1,9 +1,12 @@
 package com.simmya.mapper;
 
+import org.springframework.stereotype.Repository;
+
 import com.simmya.pojo.Discuss;
 
 import tk.mybatis.mapper.common.Mapper;
 
+@Repository
 public interface DiscussMapper extends Mapper<Discuss> {
     
 }
--- src/main/java/com/simmya/mapper/OrdersMapper.java
+++ src/main/java/com/simmya/mapper/OrdersMapper.java
@@ -1,9 +1,12 @@
 package com.simmya.mapper;
 
+import org.springframework.stereotype.Repository;
+
 import com.simmya.pojo.Orders;
 
 import tk.mybatis.mapper.common.Mapper;
 
+@Repository
 public interface OrdersMapper extends Mapper<Orders> {
    
 }
